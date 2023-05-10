1a420a7e9610ef9538241ce7b831037f2e3cae7c
Abdoulllahi <dabdoulahi@gmail.com>
2023-05-10 14:21:08 -0500

    html validator corrected

---
+++ assignment8/question1.js
@@ -0,0 +1,9 @@
+const student = {
+    firstName: "",
+    lastName: "",
+    grades: [],
+    inputNewGrade: function (grade) {
+        this.grades.push(grade)
+    },
+    computeAverageGrade: () =
+}
