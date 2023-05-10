f26063115e711187af9c3412b5801514ee78bb85
Abdoulllahi <dabdoulahi@gmail.com>
2023-05-10 15:02:33 -0500

    css and html validators corrected for assignment 3

--- assignment8/question1.js
+++ assignment8/question1.js
@@ -5,4 +5,26 @@ const student = {
     inputNewGrade: function (grade) {
         this.grades.push(grade)
     },
+    computeAverageGrade: function () {
+        return this.grades.reduce((sum, grade) => sum += grade, 0) / this.grades.length;
+    }
+}
+
+const firstNames = ["Jean", "Claude", "Anna", "Isabella", "John", "Abdu"];
+const lastNames = ["Smith", "Certail", "Jordan", "Dupont", "Endrics"];
+const student1 = Object.create(student);
+student1.firstName = 'John';
+student1.lastName = 'Smith';
+student1.inputNewGrade(89);
+student1.inputNewGrade(45);
+const student2 = Object.create(student);
+const student3 = Object.create(student);
+const student4 = Object.create(student);
+
+const students = [student1, student2, student3, student4];
+console.log(students)
+console.log(student1.grades)
+console.log(student1.computeAverageGrade())
+for (let i = 0; i < 5; i++) {
+    console.log(Math.round(Math.random() * 10));
 }
