d9d198c8c920896ae6c0cf9562340a412c10462a
Abdoulllahi <dabdoulahi@gmail.com>
2023-05-13 21:21:40 -0500

    assignment 9

--- assignment9/question.js
+++ assignment9/question.js
@@ -1,16 +1,16 @@
 class Student {
 
-    static studentIds = [];
+    static studentIds = []
     constructor(studentId, answers) {
-        if (Student.studentIds.includes(studentId)) {
-            console.log("Student id must be unique!!!");
+        if (studentIds.includes(studentId)) {
             this.studentId = undefined;
             this.answers = undefined;
+            console.log("Student ids must be unique");
             return;
         }
         this.studentId = studentId;
         this.answers = answers;
-        Student.studentIds.push(this.studentId);
+        Student.studentIds.push(studentId);
     }
 
     addAnswer(question) {
@@ -20,32 +20,50 @@ class Student {
 
 class Question {
 
-    static qIds = [];
-    constructor(qId, answer) {
-        if (Question.qIds.includes(qId)) {
-            console.log("Question ids must be unique!!!");
-            this.qId = undefined;
+    static qids = [];
+    constructor(qid, answer) {
+        if (qids.includes(qid)) {
+            this.qid = undefined;
             this.answer = undefined;
+            console.log("Questions Ids must be unique");
             return;
         }
-        this.qId = qId;
+        this.qid = qid;
         this.answer = answer;
-        Question.qIds.push(qId);
+        Question.qids.push(qid);
     }
 
     checkAnswer(answer) {
-        return this.answer == answer;
+        return this.answer === answer;
     }
 }
 
 class Quiz {
 
-    constructor(questions, students) {
-        this.questions = questions;
+    constructor(questionArray, students) {
+        this.questions = new Map();
+        questionArray.array.forEach(question => this.questions.set(question.qid, question.answer));
         this.students = students;
     }
 
+    scoreStudent(studentId) {
+
+        let student = this.students.filter(student => student.studentId === studentId)[0];
+        return student.answers.reduce((score, currentQuestion) => {
+            let questionId = currentQuestion.questionId; //find quesiton id
+            let correctAnswer = this.questions.get(questionId); //get correctAnswer from Map
+            let result = currentQuestion.checkAnswer(correctAnswer); //compare currentQuestion answer with correctAnswer
+            if (result) {
+                score = sscore + 1;
+            }
+            return score;
+        }, 0);
+    }
+
     getAverageScore() {
-        
+        return this.students.reduce((average, currentStudent, index, array) =>
+            average + this.scoreStudent(currentStudent.studentId) / array.length, 0);
+
     }
 }
+
